709857cecf6310a3c5aea310b344c8677a627d1c
Matthew Zhao <matthew@mail.matthew-cloud.com>
2021-02-08 12:14:03 -0800

    100 times mean

--- measureapi/lambda_handler.py
+++ measureapi/lambda_handler.py
@@ -1,6 +1,9 @@
 import requests
 
 def lambda_handler(event, context):
+    sum = 0
+    for _ in range(100):
+        sum += requests.get(event["url"]).elapsed.total_seconds()
     return {
-        "latency": requests.get(event["url"]).elapsed.total_seconds()
+        "latency": sum/100
     }
