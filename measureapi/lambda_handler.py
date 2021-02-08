66d9a59c55663d3cd386d03be49a8367702cbe86
Matthew Zhao <matthew@mail.matthew-cloud.com>
2021-02-08 12:23:11 -0800

    10 req mean not 100

--- measureapi/lambda_handler.py
+++ measureapi/lambda_handler.py
@@ -2,7 +2,7 @@ import requests
 
 def lambda_handler(event, context):
     sum = 0
-    for _ in range(100):
+    for _ in range(10):
         sum += requests.get(event["url"]).elapsed.total_seconds()
     return {
         "latency": sum/100
