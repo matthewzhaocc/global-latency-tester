acbcd628934c13a1c1e81b6b4148b39f797f12ae
Matthew Zhao <matthew@mail.matthew-cloud.com>
2021-02-07 22:45:55 -0800

    code xd

--- measureapi/lambda_handler.py
+++ measureapi/lambda_handler.py
@@ -2,11 +2,5 @@ import requests
 
 def lambda_handler(event, context):
     return {
-        "latency": requests.get(event["url"]).elapsed.total_seconds()
+        "latency": requests.get(context["url"]).elapsed.total_seconds()
     }
-
-if __name__ == "__main__":
-    event = {
-        "url":"https://google.com"
-    }
-    print(lambda_handler(event, ""))
---
+++ measurelatency.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+import json
+import boto3
+
+def invoke_lambda(region):
+    lambda_client = boto3.client("lambda", region_name = region)
+    lambda_payload = json.dumps({"url": sys.argv[1]})
+    return lambda_client.invoke(FunctionName='apimeasure', 
+                     InvocationType='RequestResponse',
+                     Payload=lambda_payload)
+regions = ["us-east-1", "us-east-2", "us-west-1", "us-west-2", "ap-south-1", "ap-northeast-2", "ap-northeast-1", "ap-southeast-2", "eu-central-1", "eu-west-2", "eu-west-3", "eu-north-1", "sa-east-1"]
+for region in regions:
+    latency = json.loads(invoke_lambda(region)["Payload"].read().decode("utf-8"))["latency"]
+    print(f"the latency measured in region: {region} for website {sys.argv[1]} is {latency} seconds")
+    
