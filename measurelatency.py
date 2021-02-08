49695db17de529c40916d87de783822f7a8f1aff
Matthew Zhao <matthew@mail.matthew-cloud.com>
2021-02-08 11:22:14 -0800

    save my eyes

--- measurelatency.py
+++ measurelatency.py
@@ -9,7 +9,20 @@ def invoke_lambda(region):
     return lambda_client.invoke(FunctionName='apimeasure', 
                      InvocationType='RequestResponse',
                      Payload=lambda_payload)
-regions = ["us-east-1", "us-east-2", "us-west-1", "us-west-2", "ap-south-1", "ap-northeast-2", "ap-northeast-1", "ap-southeast-2", "eu-central-1", "eu-west-2", "eu-west-3", "eu-north-1", "sa-east-1"]
+regions = ["us-east-1",
+        "us-east-2", 
+        "us-west-1", 
+        "us-west-2", 
+        "ap-south-1", 
+        "ap-northeast-2", 
+        "ap-northeast-1", 
+        "ap-southeast-2", 
+        "eu-central-1", 
+        "eu-west-2", 
+        "eu-west-3", 
+        "eu-north-1", 
+        "sa-east-1"
+        ]
 for region in regions:
     latency = json.loads(invoke_lambda(region)["Payload"].read().decode("utf-8"))["latency"]
     print(f"the latency measured in region: {region} for website {sys.argv[1]} is {latency} seconds")
