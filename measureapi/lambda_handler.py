44f755a739e84391afc0af096da14bb552793d9b
Matthew Zhao <matthew@mail.matthew-cloud.com>
2021-02-08 17:06:36 -0800

    reduce to 1

--- measureapi/lambda_handler.py
+++ measureapi/lambda_handler.py
@@ -2,8 +2,8 @@ import requests
 
 def lambda_handler(event, context):
     sum = 0
-    for _ in range(10):
+    for _ in range(1):
         sum += requests.get(event["url"]).elapsed.total_seconds()
     return {
-        "latency": sum/100
+        "latency": sum
     }
